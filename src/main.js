3a342eeb56f2f8af1f61355f9335cc12961d1d88
joffreyverd <jverd@protonmail.com>
2024-08-25 18:13:05 +0200

    Feature: first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import './assets/index.css';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(createPinia());
+
+app.mount("#app");
